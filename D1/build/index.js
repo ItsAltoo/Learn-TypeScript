29141592f8fc73ba930278d5c823d295320b01f7
ItsAltoo <Itsmalik57@gmail.com>
2024-12-21 20:28:20 +0800

    21.12.24

--- D1/build/index.js
+++ D1/build/index.js
@@ -10,6 +10,7 @@ namaSaya = ["Malik", "akbar"];
 let dataMahasiswa;
 dataMahasiswa = [1250, "mancap", false];
 let friends;
+// TIPE DATA CUSTOM
 friends = {
     name: "Node",
     Absen: true,
@@ -36,3 +37,75 @@ const sayHello = (name) => {
 console.log(create());
 console.log(math());
 sayHello("wanjay");
+console.log("====================================");
+// Function
+function add(a, b) {
+    return a + b;
+}
+// Arrow Function
+const multiply = (a, b) => {
+    return a * b;
+};
+console.log(add(10, 20), multiply(10, 20));
+console.log(typeof add, typeof multiply);
+console.log("====================================");
+// Array
+let arr;
+arr = ["1", "2", "3", "4", "5"];
+console.log(arr);
+console.log(typeof arr);
+// mengapa hasilnya object? karena array di javascript adalah object
+// untuk mengecek tipe data array bisa menggunakan Array.isArray(arr)
+console.log("====================================");
+// Object
+let obj;
+obj = { name: "Malik", age: 18 };
+console.log(obj);
+console.log(typeof obj);
+console.log("====================================");
+let person;
+person = { name: "Malik", age: 18, nim: 1230 };
+console.log(person);
+console.log(typeof person);
+// interface bisa dijadikan sebagai tipe data custom yang bisa digunakan untuk menentukan tipe data dari sebuah variabel atau parameter.
+// perbedaan interface dengan type adalah interface hanya bisa digunakan untuk object sedangkan type bisa digunakan untuk object, array, function, dll.
+console.log("====================================");
+// Union
+let phone;
+phone = 628123456789;
+console.log(phone);
+phone = "08123456789";
+console.log(phone);
+console.log(typeof phone);
+// Union adalah tipe data yang bisa memiliki lebih dari satu tipe data.
+// apakah bisa menggunakan union pada array? bisa, tapi tidak disarankan karena akan membuat kode menjadi ambigu.
+// jadi sebaiknya gunakan union pada tipe data yang sederhana seperti number, string, boolean, dll.
+console.log("====================================");
+let phoneNumber;
+phoneNumber = 628123456789;
+console.log(phoneNumber);
+phoneNumber = "08123456789";
+console.log(phoneNumber);
+console.log(typeof phoneNumber);
+let user;
+user = { name: "Malik", age: 18, nim: 1230 };
+console.log(user);
+console.log(typeof user);
+console.log("====================================");
+let users;
+users = ["Malik", "Akbar"];
+console.log(users);
+console.log(typeof users);
+console.log("====================================");
+let ade;
+ade = (a, b) => a + b;
+console.log(ade(10, 20));
+console.log(typeof ade);
+// Type Alias pada function bisa digunakan untuk menentukan tipe data dari parameter dan return value dari function.
+//apakah bisa memanggil tanpa let ade: Add; ? bisa, tapi tidak disarankan karena akan membuat kode menjadi ambigu.
+// jadi sebaiknya gunakan let ade: Add; untuk mempermudah pembacaan kode.
+// contohnya seperti ini
+let ad = (a, b) => a + b;
+console.log(ad(10, 20));
+console.log(typeof ad);
+console.log("====================================");
--- D1/src/index.ts
+++ D1/src/index.ts
@@ -22,6 +22,7 @@ type Friend = {
 
 let friends: Friend;
 
+// TIPE DATA CUSTOM
 friends = {
   name: "Node",
   Absen: true,
@@ -54,3 +55,112 @@ const sayHello = (name: string) => {
 console.log(create());
 console.log(math());
 sayHello("wanjay");
+
+console.log("====================================");
+
+// Function
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+// Arrow Function
+const multiply = (a: number, b: number): number => {
+  return a * b;
+};
+
+console.log(add(10, 20),multiply(10, 20));
+console.log(typeof add,typeof multiply);
+
+console.log("====================================");
+// Array
+let arr: string[];
+arr = ["1", "2", "3", "4", "5"];
+console.log(arr);
+console.log(typeof arr);
+// mengapa hasilnya object? karena array di javascript adalah object
+// untuk mengecek tipe data array bisa menggunakan Array.isArray(arr)
+console.log("====================================");
+
+
+// Object
+let obj: { name: string; age: number };
+obj = { name: "Malik", age: 18 };
+console.log(obj);
+console.log(typeof obj);
+
+console.log("====================================");
+
+// Interface
+interface Person {
+  name: string;
+  age: number;
+  nim: number;
+}
+let person: Person;
+person = { name: "Malik", age: 18,nim: 1230 };
+console.log(person);
+console.log(typeof person);
+// interface bisa dijadikan sebagai tipe data custom yang bisa digunakan untuk menentukan tipe data dari sebuah variabel atau parameter.
+// perbedaan interface dengan type adalah interface hanya bisa digunakan untuk object sedangkan type bisa digunakan untuk object, array, function, dll.
+console.log("====================================");
+
+// Union
+let phone: number | string;
+phone = 628123456789;
+console.log(phone);
+phone = "08123456789";
+console.log(phone);
+console.log(typeof phone);
+// Union adalah tipe data yang bisa memiliki lebih dari satu tipe data.
+// apakah bisa menggunakan union pada array? bisa, tapi tidak disarankan karena akan membuat kode menjadi ambigu.
+// jadi sebaiknya gunakan union pada tipe data yang sederhana seperti number, string, boolean, dll.
+console.log("====================================");
+
+// Type Alias
+type Phone = number | string;
+let phoneNumber: Phone;
+phoneNumber = 628123456789;
+console.log(phoneNumber);
+phoneNumber = "08123456789";
+console.log(phoneNumber);
+console.log(typeof phoneNumber);
+// Type Alias adalah tipe data custom yang bisa digunakan untuk menentukan tipe data dari sebuah variabel atau parameter.
+// Type Alias bisa digunakan untuk object, array, function, dll.
+// contoh penggunaan Type Alias pada object
+type User = {
+  name: string;
+  age: number;
+  nim: number;
+};
+let user: User;
+user = { name: "Malik", age: 18, nim: 1230 };
+console.log(user);
+console.log(typeof user);
+console.log("====================================");
+// contoh penggunaan Type Alias pada array
+type Users = string[];
+let users: Users;
+users = ["Malik", "Akbar"];
+console.log(users);
+console.log(typeof users);
+console.log("====================================");
+// contoh penggunaan Type Alias pada function
+type Add = (a: number, b: number) => number;
+let ade: Add;
+ade = (a, b) => a + b;
+console.log(ade(10, 20));
+console.log(typeof ade);
+// Type Alias pada function bisa digunakan untuk menentukan tipe data dari parameter dan return value dari function.
+//apakah bisa memanggil tanpa let ade: Add; ? bisa, tapi tidak disarankan karena akan membuat kode menjadi ambigu.
+// jadi sebaiknya gunakan let ade: Add; untuk mempermudah pembacaan kode.
+// contohnya seperti ini
+let ad: Add = (a, b) => a + b;
+console.log(ad(10, 20));
+console.log(typeof ad);
+// mengapa harus menggunakan let ad: Add; ? karena dengan menggunakan let ad: Add; kita bisa mengetahui bahwa ad adalah variabel yang bertipe data Add.
+// apa yang terjadi jika let di ganti type? akan error karena type tidak dikenali oleh typescript.
+// apakah bisa langsung memanggil type add tanpa let? bisa, tapi tidak disarankan karena akan membuat kode menjadi ambigu.
+// contohnya seperti ini
+// (a, b) => a + b;
+// console.log(add(10, 20));  
+console.log("====================================");
